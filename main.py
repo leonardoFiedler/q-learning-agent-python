17c9668e9c8403dfdf2292225753bc1e90f6046b
Leonardo Fiedler <leonardo.fiedler.96@gmail.com>
2021-06-28 21:32:45 -0300

    Main impl

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 # 6x7 grid
 # 0 - Current agent position
@@ -20,10 +21,10 @@ ENVIRONMENT_ROWS = 6
 ENVIRONMENT_COLUMNS = 7
 INITIAL_AGENT_POS = (5,0)
 INITIAL_OBJECT_POS = (2,3)
-ACTIONS = ['up', 'right', 'down', 'left']
+ACTIONS = ['up', 'right', 'down', 'left', 'stay']
 TERMINAL_STATE_INDEX = {0: [2, 3, 4]}
 
-q_values = np.zeros((ENVIRONMENT_ROWS, ENVIRONMENT_COLUMNS, 4))
+q_values = np.zeros((ENVIRONMENT_ROWS, ENVIRONMENT_COLUMNS, 5))
 
 # Define todos os lugares em que o agente nao pode se mover
 walls = {}
@@ -33,15 +34,21 @@ walls[4].extend([i for i in range(3, 7)])
 walls[5] = [6]
 
 # Define todos os locais inicialmente como -100
-rewards = np.full((ENVIRONMENT_ROWS, ENVIRONMENT_COLUMNS), -100.)
+# O tamanho do array de recompensas é 6x7x2
+# O tamanho 2 ao final foi definido para enquadrar as situacoes
+# em que o objeto esta selecionado ou nao, sendo
+# 0 - objeto nao esta anexado / 1 - objeto esta anexado
+rewards = np.full((ENVIRONMENT_ROWS, ENVIRONMENT_COLUMNS, 2), -100.)
 
-# Define os pontos da base e ao lado do objeto como alguma recompensa maior
-rewards[0, 2] = 100
-rewards[0, 3] = 100
-rewards[0, 4] = 100
+# Quando o objeto nao esta selecionado
+# Adiciona nas duas laterais o valor de recompensa de 50
+rewards[2, 2, 0] = 50
+rewards[2, 4, 0] = 50
 
-# rewards[2, 2] = 50
-# rewards[2, 4] = 50
+# Quando o objeto esta selecionado, adiciona 100 nos campos da base
+rewards[0, 2, 1] = 100
+rewards[0, 3, 1] = 100
+rewards[0, 4, 1] = 100
 
 # Define os espacos possiveis de caminhar com uma recompensa de -1
 aisles = {}
@@ -58,8 +65,6 @@ aisles[5] = [i for i in range(0, 6)]
 for row_index in range(0, 6):
     for col_index in aisles[row_index]:
         rewards[row_index, col_index] = -1
-
-print(rewards)
 
 def visualize_environment(
     current_row_position, current_col_position,
@@ -83,7 +88,7 @@ def visualize_environment(
         print(txt)
         print('-'*40)
     
-    # time.sleep(1)
+    # time.sleep(0.3)
 
 def is_terminal_state(object_row_index, object_column_index):
     if object_row_index in TERMINAL_STATE_INDEX.keys():
@@ -127,76 +132,66 @@ def get_next_location(current_row_index, current_column_index, action_index):
         new_row_index += 1
     elif ACTIONS[action_index] == 'left' and current_column_index > 0:
         new_column_index -= 1
-    
+
     return new_row_index, new_column_index
 
-def get_shortest_path():
-    current_row_index, current_column_index = INITIAL_AGENT_POS
-    current_object_row_index, current_object_column_index = INITIAL_OBJECT_POS
-    
-    object_attached = False
-    object_right_side = False
+def plot_convergence_curve(convergence_data):
+    x = convergence_data.keys()
+    y = convergence_data.values()
+    plt.scatter(x,y)
+    plt.plot(x,y)
 
-    shortest_path = []
-    shortest_path.append([current_row_index, current_column_index])
-
-    while not is_terminal_state(current_object_row_index, current_object_column_index):
-        action_index = get_next_action(current_row_index, current_column_index, 1.)
-        current_row_index, current_column_index = get_next_location(
-            current_row_index, current_column_index, action_index
-        )
-
-        shortest_path.append([current_row_index, current_column_index])
-    
-    return shortest_path
+    plt.title('Curva de Convergência - Número total de timesteps por episódio')
+    plt.xlabel('Episódios')
+    plt.ylabel('Timesteps')
+    plt.savefig('Curva de Convergência.png')
 
 def main():
-    epsilon = 0.9
-    discount_factor = 0.9
-    learning_rate = 0.9
-    max_timesteps_per_episode = 100
+    epsilon = 0.3 # e
+    discount_factor = 0.9 # γ - gamma
+    learning_rate = 0.9 # a
+    max_timesteps_per_episode = 200
+    visualize = False
+    start_time = time.time()
+
+    convergence_curve = {}
 
     for episode in range(10):
         row_index, column_index = INITIAL_AGENT_POS
         object_row_index, object_column_index = INITIAL_OBJECT_POS
 
-        object_attached = False
-        object_right_side = False
+        object_attached = 0
 
         timesteps = 0
-        visualize_environment(
-            row_index, column_index,
-            object_row_index, object_column_index
-        )
+
+        if visualize:
+            visualize_environment(
+                row_index, column_index,
+                object_row_index, object_column_index
+            )
         
         print('Episodio: {}'.format(episode))
         # time.sleep(1)
 
         while not is_terminal_state(object_row_index, object_column_index):
-            if object_attached:
-                action_found = False
-                while not action_found:
-                    action_index = get_next_action(row_index, column_index, epsilon)
-                    if object_right_side and ACTIONS[action_index] != 'right':
-                        action_found = True
-                    elif not object_right_side and ACTIONS[action_index] != 'left':
-                        action_found = True
-            else:    
-                action_index = get_next_action(row_index, column_index, epsilon)
+            action_index = get_next_action(row_index, column_index, epsilon)
             
             old_row_index, old_column_index = row_index, column_index
             old_obj_row_index, old_obj_col_index = object_row_index, object_column_index
 
             row_index, column_index = get_next_location(row_index, column_index, action_index)
 
-            if object_attached:
+            if object_attached > 0:
                 object_row_index, object_column_index = get_next_location(object_row_index, object_column_index, action_index)
-
-            reward = rewards[row_index, column_index]
+            
+            reward = rewards[row_index, column_index, object_attached]
+            
             old_q_value = q_values[old_row_index, old_column_index, action_index]
-            temporal_difference = reward + (discount_factor * np.max(q_values[row_index, column_index])) - old_q_value
 
+            # Calculo do Epsilon greedy strategy
+            temporal_difference = reward + ((discount_factor * np.max(q_values[row_index, column_index])) - old_q_value)
             new_q_value = old_q_value + (learning_rate * temporal_difference)
+
             q_values[old_row_index, old_column_index, action_index] = new_q_value
 
             if (is_invalid_state(row_index, column_index, object_row_index, object_column_index)):
@@ -206,24 +201,29 @@ def main():
                 if not object_attached:
                     if row_index == INITIAL_OBJECT_POS[0]:
                         if (column_index + 1) == INITIAL_OBJECT_POS[1]:
-                            object_attached = True
-                            object_right_side = True
+                            object_attached = 1
                         elif (column_index - 1) == INITIAL_OBJECT_POS[1]:
-                            object_attached = True
-                            object_right_side = False
+                            object_attached = 1
 
             timesteps += 1
-            visualize_environment(
-                row_index, column_index,
-                object_row_index, object_column_index
-            )
 
-            if timesteps > max_timesteps_per_episode:
+            if visualize:
+                visualize_environment(
+                    row_index, column_index,
+                    object_row_index, object_column_index
+                )
+
+            if timesteps > (max_timesteps_per_episode - 1):
                 print('Quantidade maxima atingida. Reiniciando...')
                 # time.sleep(1)
                 break
-    
-    print('Training complete')
-    path = get_shortest_path()
 
-main()
+        print('Etapa concluida com {0} timesteps.'.format(timesteps))
+        convergence_curve[episode] = timesteps
+    
+    end_time = time.time()
+    print('Treinamento completo em {0}'.format(end_time - start_time))
+    plot_convergence_curve(convergence_curve)
+
+if __name__ == "__main__":
+    main()
